ec711f24732d9fc07eb49bbc72443162b36fb381
Leo <leoja464@student.liu.se>
2025-04-11 22:21:05 +0200

    problem b lab 2 might be working, need to check kattis

--- labs/lab3/suffixsorting/src/main.rs
+++ labs/lab3/suffixsorting/src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     collections::{HashSet, VecDeque},
     fs,
-    io::{self, Read},
+    io::{self, Read}, usize,
 };
 
 /*
@@ -9,101 +9,138 @@ Author: Leo Jarhede
 LiuID: leoja464
 */
 
+const TOTAL: usize = 256; // The number of possible letters
+
 fn main() {
     //let file_path = "suffixsorting.in";
     //let content = fs::read_to_string(file_path).expect("Failed to read file");
 
-    let mut buffer = Vec::new();
-    io::stdin()
-        .read_to_end(&mut buffer)
-        .expect("Failed to read from stdin");
-    let content = String::from_utf8_lossy(&buffer);
+    //let mut buffer = Vec::new();
+    //io::stdin()
+    //    .read_to_end(&mut buffer)
+    //    .expect("Failed to read from stdin");
+    //let content = String::from_utf8_lossy(&buffer);
 
-    let mut lines = content.lines();
+    //let mut lines = content.lines();
 
+    println!("{:?}", build_suffix_array("Popup$".chars().collect()));
 }
-
-struct SuffixNode {
-    child: Vec<SuffixNode>, 
-    start: usize,
-    end: usize,
-    index: usize,
-    input: Vec<char>, 
-}
-
-impl SuffixNode {
-    pub fn new(start: usize, end: usize) -> Self {
-        Self {child: vec![], start: start, end: end, index: 0 } // Is this index correct 
-    }
-}
-
-struct Active {
-    active_length: usize,
-    active_node: SuffixNode,
-    active_edge: i32, 
-}
-impl Active {
-    pub fn new(node: SuffixNode) -> Self {
-        Self {active_length: 0, active_node: node, active_edge: -1} // Is this index correct 
+/*
+* if we have some alpabet of a-d
+* then we might want to get the order of the string ba
+* 
+* the count: [1, 1, 0, 0]
+* after cumsum
+* count: [1, 2, 2, 2]
+*
+* The resuting order will then be:
+* order: [1, 0]
+*/
+fn sort_characters (s: &Vec<char>) -> Vec<usize> {
+    let mut order = vec![0; s.len()];
+    let mut count = [0; TOTAL];
+    
+    for i in 0..s.len() {
+        count[s[i] as usize ] += 1;
     }
 
-struct SuffixTree {
-    root: SuffixNode,
-    active: Active,
-    remaining: usize,
-}
 
-fn select_node_index(tree: SuffixTree, index: usize) -> Option<SuffixNode>{
-    tree.active_node.child.get(tree.input[index])
-}
-
-fn select_node(tree: SuffixTree, index: usize) -> Option<SuffixNode> {
-    tree.active_node.child.get(tree.input[tree.active.active_edge])
-}
-
-fn build(tree: SuffixTree) {
-    let mut root = SuffixNode::new(1, 0);
-    root.index = -1;
-    let mut active = Active::new(root);
-    tree.end = -1;
-
-    for i in 0..tree.input.len() {
-        start_phase(tree, i);
+    for i in 1..TOTAL {
+        count[i] = count[i] + count[i - 1];
     }
+
+    for i in (0..s.len()).rev() {
+        let c = s[i] as usize;
+        count[c] -= 1;
+        order[count[c]] = i;
+    }
+
+    order
+}
+
+/*
+* Here s is the string that we want to find the classses in 
+* order is the order of the letters in the string s,
+*
+* The goal of this function is to return to us the class of each of the letters in the string s.
+* This class will represent the valid sorting partions, i.e any reodering of the letters in one
+* class will still result a valid ordering. 
+*/
+fn compute_char_classes (s: &Vec<char>, order: &Vec<usize>) -> Vec<usize> {
+    let mut class: Vec<usize> = vec![0; s.len()];
+
+    for i in 1..s.len() {
+        if s[order[i]] != s[order[i - 1]] {  // If the two letters is not then same then they
+            // must also belong to different classes 
+            class[order[i]] = class[order[i - 1]] + 1;
+        }
+        else { // They do indeed belong to the same class
+            class[order[i]] = class[order[i - 1]];
+        }
+    }
+    class
+}
+
+fn sort_double(s: &Vec<char>, l: usize, order: Vec<usize>, class: &Vec<usize>) -> Vec<usize> {
+    let mut count: Vec<usize> = vec![0; s.len()];
+    let mut new_order: Vec<usize> = vec![0; s.len()];
+
+    for i in 0..s.len() {
+        count[class[i]] += 1;
+    }
+
+    for i in 1..s.len() {
+        count[i] = count[i] + count[i - 1];
+    }
+
+    for i in (0..s.len()).rev() {
+        let start = (order[i] + (s.len() - l)) % s.len();
+        let cl = class[start];
+        count[cl] -= 1;
+        new_order[count[cl]] = start;
+    }
+
+    new_order
 }
 
 
-fn nextChar (tree: SuffixTree, i: usize) {
-    let node = select_node();
-    if 
-}
 
+fn update_class(new_order: &Vec<usize>, class: Vec<usize>, l: usize) -> Vec<usize> {
+    let n = new_order.len();
+    let mut new_class = vec![0; n];
+    
+    for i in 1..n {
+        let cur = new_order[i];
+        let prev = new_order[i - 1];
 
-fn start_phase (tree: SuffixTree, i: usize) {
-    let mut lastCreatedInternalNode: Option<SuffixNode> = None;
+        let mid = (cur + l) % n;
+        let mid_prev= (prev + l) % n;
 
-    tree.end += 1;
-
-    tree.remaining += 1;
-
-    while tree.remaining > 0 {
-        if tree.active_length == 0 {
-            if let Some(selected_node) = select_node(i) {
-                tree.active.active_edge = selected_node.start;
-                tree.active.active_length += 1;
-                break;
-            }
-            else {
-                tree.root.child[tree.input[i]] = SuffixNode::new(i, end);
-                tree.remaining -= 1;
-            }
+        if class[cur] != class[prev] || class [mid] != class[mid_prev] {
+            new_class[cur] = new_class[prev] + 1;
         }
         else {
-
+            new_class[cur] = new_class[prev];
         }
     }
 
-
-    
-
+    new_class
 }
+
+fn build_suffix_array(s: Vec<char>) -> Vec<usize> {
+    let s_len  = s.len();
+    let mut order = sort_characters(&s);
+    let mut class = compute_char_classes(&s, &order);
+    println!("{:?}", class);
+
+    let mut l = 1;
+    while l < s_len { 
+        order = sort_double(&s, l, order, &class);
+        class = update_class(&order , class, l);
+
+        l *= 2;
+    }
+
+    order
+}
+
